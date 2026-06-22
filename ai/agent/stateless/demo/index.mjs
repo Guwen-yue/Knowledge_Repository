96414ebf14839f8ac352ab1e2d425f00420d3b59
Guwen-yue <2606633761@qq.com>
2026-06-22 21:25:35 +0800

    2026-6-22 “context上下文”

---
+++ ai/agent/stateless/demo/index.mjs
@@ -0,0 +1,48 @@
+import OpenAI from 'openai';
+import { config } from 'dotenv';
+config();
+
+const client = new OpenAI({
+  apiKey: process.env.DEEPSEEK_API_KEY,
+  baseURL: process.env.DEEPSEEK_BASE_URL,
+})
+// 异步任务
+// promise 实例
+
+// 对话历史
+const chatHistory = []
+async function testStateless() {
+  console.log('第一次请求，告诉模型一个信息');
+  chatHistory.push({
+    role: 'user',
+    content: '请记住， 我的名字字节胡'
+  })
+  const response = await client.chat.completions.create({
+    model: 'deepseek-v4-flash',
+    messages: chatHistory
+  });
+  chatHistory.push({
+    role: 'assistant',
+    content: "请问我我的名字是什么？"
+  })
+  console.log('模型回复：', 
+    response.choices[0].message.content);
+  console.log('第二次请求，直接问我是谁');
+  const response2 = await client.chat.completions.create({
+    model: 'deepseek-v4-flash', 
+    messages: chatHistory
+  });
+  chatHistory.push({
+    role: 'assistant',
+    content: response2.choices[0].message.content
+  })
+  console.log('模型回复：', 
+    response2.choices[0].message.content);
+    console.log(chatHistory);
+}
+
+// console.log(testStateless())
+testStateless()
+  .catch(err => { // test函数 reject 
+    console.log(err);
+  })
